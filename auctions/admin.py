a64cb3e7ea5814794d3711e68bca78dc200d540e
Chee Kang Tan <cheekangt@gmail.com>
2021-01-02 15:35:47 -0600

    Add files via upload

---
+++ auctions/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Bid, Comment, Listing, User, Watchlist
+#,Category
+
+# Register your models here.
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Listing)
+admin.site.register(User)
+admin.site.register(Watchlist)
+#admin.site.register(Category)
